7d744bf43c8f0a1bcb15961d52a157f5166cc218
david <leachda12@icloud.com>
2024-11-20 16:08:44 -0500

    initial commit

---
+++ index_cards.py
@@ -0,0 +1,110 @@
+import json
+from glob import glob
+from elasticsearch import Elasticsearch
+
+client = Elasticsearch("http://localhost:9200")
+
+filenames = glob("AllSetFiles/*.json")
+
+fields_to_keep = {
+    "borderColor": None,
+    "cardParts": None,
+    "colorIdentity": None,
+    "colors": None,
+    "convertedManaCost": None,
+    "defense": None,
+    "flavorName": None,
+    "flavorText": None,
+    "identifiers": {
+        "scryfallId": None,
+        "scryfallIllustrationId": None,
+        "tcgplayerProductId": None,
+    },
+    "keywords": None,
+    "leadershipSkills": {"brawl": None, "commander": None, "oathbreaker": None},
+    "legalities": None,
+    "life": None,
+    "loyalty": None,
+    "manaCost": None,
+    "name": None,
+    "number": None,
+    "otherFaceIds": None,
+    "power": None,
+    "rarity": None,
+    "releaseDate": None,
+    "setCode": None,
+    "subtypes": None,
+    "supertypes": None,
+    "text": None,
+    "toughness": None,
+    "type": None,
+    "types": None,
+    "uuid": None,
+}
+
+
+def get_set_info(set_data):
+    set_info = set_data.copy()
+    set_info.pop("cards", None)
+    set_info.pop("tokens", None)
+    set_info.pop("booster", None)
+    return set_info
+
+
+def filter_fields(data, fields_to_keep):
+    if isinstance(data, dict):
+        return {
+            key: (
+                filter_fields(value, fields_to_keep[key])
+                if isinstance(fields_to_keep.get(key), dict)
+                else value
+            )
+            for key, value in data.items()
+            if key in fields_to_keep
+        }
+    elif isinstance(data, list):
+        return [filter_fields(item, fields_to_keep) for item in data]
+    else:
+        return data
+
+
+def index_set(filename, index_name):
+    print(f"Indexing {filename}...")
+    with open(filename, "r", encoding="utf-8") as file:
+        data = json.load(file)
+
+    # set info
+    set_info = get_set_info(data["data"])
+
+    # ingest cards
+    for card in data["data"].get("cards", []):
+        document = filter_fields(card, fields_to_keep)
+        document["set_info"] = set_info
+        client.index(index=index_name, document=document)
+
+    # ingest tokens
+    for token in data["data"].get("tokens", []):
+        document = filter_fields(token, fields_to_keep)
+        document["set_info"] = set_info
+        client.index(index=index_name, document=document)
+
+
+def main():
+    filename = "AllSetFiles/AMH1.json"
+    index_name = "mtg_cards"
+
+    # ? Does this check really need to be here?
+    # check if index exists
+    if not client.indices.exists(index=index_name):
+        # if not, create index
+        print(f'Index does not exist, creating index "{index_name}"')
+        client.indices.create(index=index_name)
+
+    for file in filenames:
+        index_set(file, index_name)
+
+    print("Done!")
+
+
+if __name__ == "__main__":
+    main()
